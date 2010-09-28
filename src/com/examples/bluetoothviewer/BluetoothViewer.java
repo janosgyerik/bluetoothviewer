d9046ac52b683d9b544a10f2c0a351cdba0ecc8c
Janos Gyerik <janos@wall-e>
2010-09-29 08:23:42 +0900

    append newline character when sending messages, perhaps this will make it work

--- src/com/examples/bluetoothviewer/BluetoothViewer.java
+++ src/com/examples/bluetoothviewer/BluetoothViewer.java
@@ -230,7 +230,7 @@ public class BluetoothViewer extends Activity {
             // If the action is a key-up event on the return key, send the message
             if (actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_UP) {
                 String message = view.getText().toString();
-                sendMessage(message);
+                sendMessage(message + "\n");
             }
             if(D) Log.i(TAG, "END onEditorAction");
             return true;
