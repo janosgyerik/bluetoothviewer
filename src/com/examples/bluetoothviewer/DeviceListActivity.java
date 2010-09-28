0f1b0c7a649b9efbbbe201552dd13aab5a485062
Janos Gyerik <janos@wall-e>
2010-09-29 08:10:54 +0900

    fixed the bug of dubplicate listing of bluetooth devices when scanning

--- src/com/examples/bluetoothviewer/DeviceListActivity.java
+++ src/com/examples/bluetoothviewer/DeviceListActivity.java
@@ -16,11 +16,10 @@
 
 package com.examples.bluetoothviewer;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import android.app.Activity;
-import backport.android.bluetooth.BluetoothAdapter;
-import backport.android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -36,6 +35,8 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
+import backport.android.bluetooth.BluetoothAdapter;
+import backport.android.bluetooth.BluetoothDevice;
 
 /**
  * This Activity appears as a dialog. It lists any paired devices and
@@ -55,6 +56,9 @@ public class DeviceListActivity extends Activity {
     private BluetoothAdapter mBtAdapter;
     private ArrayAdapter<String> mPairedDevicesArrayAdapter;
     private ArrayAdapter<String> mNewDevicesArrayAdapter;
+    private Set<String> mNewDevicesSet;
+    
+    private Button scanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,7 +72,7 @@ public class DeviceListActivity extends Activity {
         setResult(Activity.RESULT_CANCELED);
 
         // Initialize the button to perform device discovery
-        Button scanButton = (Button) findViewById(R.id.button_scan);
+        scanButton = (Button) findViewById(R.id.button_scan);
         scanButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
                 doDiscovery();
@@ -80,6 +84,7 @@ public class DeviceListActivity extends Activity {
         // one for newly discovered devices
         mPairedDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
         mNewDevicesArrayAdapter = new ArrayAdapter<String>(this, R.layout.device_name);
+        mNewDevicesSet = new HashSet<String>();
 
         // Find and set up the ListView for paired devices
         ListView pairedListView = (ListView) findViewById(R.id.paired_devices);
@@ -135,6 +140,10 @@ public class DeviceListActivity extends Activity {
      */
     private void doDiscovery() {
         if (D) Log.d(TAG, "doDiscovery()");
+        
+        // crear results of a previous discovery
+        mNewDevicesArrayAdapter.clear();
+        mNewDevicesSet.clear();
 
         // Indicate scanning in the title
         setProgressBarIndeterminateVisibility(true);
@@ -185,16 +194,21 @@ public class DeviceListActivity extends Activity {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 // If it's already paired, skip it, because it's been listed already
                 if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
-                    mNewDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                	String address = device.getAddress();
+                	if (! mNewDevicesSet.contains(address)) {
+                		mNewDevicesSet.add(address);
+                		mNewDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                	}
                 }
             // When discovery is finished, change the Activity title
             } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
                 setProgressBarIndeterminateVisibility(false);
                 setTitle(R.string.select_device);
-                if (mNewDevicesArrayAdapter.getCount() == 0) {
+                if (mNewDevicesSet.isEmpty()) {
                     String noDevices = getResources().getText(R.string.none_found).toString();
                     mNewDevicesArrayAdapter.add(noDevices);
                 }
+                scanButton.setVisibility(View.VISIBLE);
             }
         }
     };
