30625017fb58dc3871628567ceb973e51663030d
Janos Gyerik <info@janosgyerik.com>
2014-02-14 21:29:03 +0100

    simplified getting address from device list activity

--- btviewer/src/main/java/net/bluetoothviewer/BluetoothViewer.java
+++ btviewer/src/main/java/net/bluetoothviewer/BluetoothViewer.java
@@ -340,8 +340,7 @@ public class BluetoothViewer extends Activity {
                 // When DeviceListActivity returns with a device to connect
                 if (resultCode == Activity.RESULT_OK) {
                     // Get the device MAC address
-                    String address = data.getExtras()
-                            .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+                    String address = data.getStringExtra(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
                     // Get the BluetoothDevice object
                     BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
                     // Attempt to connect to the device
