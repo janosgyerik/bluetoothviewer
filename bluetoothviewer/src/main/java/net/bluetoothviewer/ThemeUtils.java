5d10f39d494e9c8b9c1577439e350127645de44e
Janos Gyerik <janos@kronos>
2014-11-22 10:00:26 +0100

    minor cleanup in ThemeUtils

--- bluetoothviewer/src/main/java/net/bluetoothviewer/ThemeUtils.java
+++ bluetoothviewer/src/main/java/net/bluetoothviewer/ThemeUtils.java
@@ -4,19 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 
 public class ThemeUtils {
-    public final static int THEME_WHITE = 0;
-    public final static int THEME_BLACK = 1;
 
      //TODO: Use Shared Preferences to store the current theme of the app
-    private static Themes mTheme = Themes.BLACK;
+    private static Theme currentTheme = Theme.BLACK;
 
-    private static enum Themes {
-        BLACK, WHITE;
+    private static enum Theme {
+        BLACK, WHITE
     }
 
     /** Set the theme of the activity, according to the configuration. */
     public static void onActivityCreateSetTheme(Activity activity) {
-        switch (mTheme) {
+        switch (currentTheme) {
             case BLACK:
                 activity.setTheme(R.style.LightTheme);
                 break;
@@ -30,12 +28,12 @@ public class ThemeUtils {
      * Set the theme of the Activity, and restart it by creating a new Activity of the same type.
      */
     public static void cycleThemes(Activity activity) {
-        mTheme = Themes.values()[(mTheme.ordinal() + 1) % Themes.values().length];
+        currentTheme = Theme.values()[(currentTheme.ordinal() + 1) % Theme.values().length];
         restartActivity(activity);
     }
 
     private static void restartActivity(Activity activity) {
-        activity.finish(); // Finish activity in order to recreate activity with new theme
+        activity.finish();
         activity.startActivity(new Intent(activity, activity.getClass()));
     }
 }
